f6aaafc6e2d3a3cb16b3f1100df6432bf8c8940a
farazatarodi <farazatarodi@gmail.com>
2022-08-10 16:36:14 +0200

    add mode input

---
+++ src/deman_controller/src/controller_node.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Int16
+
+if __name__ == '__main__':
+    rospy.init_node('controller_node')
+
+    modePub = rospy.Publisher('/mode', Int16, queue_size=10)
+    
+    while not rospy.is_shutdown():
+        mode = input('Mode [0 for passive, 2 for position]: ')
+        if mode == '0':
+            modePub.publish(0)
+            rospy.loginfo('Mode set to passive')
+        elif mode == '2':
+            modePub.publish(2)
+            rospy.loginfo('Mode set to position')
+        else:
+            rospy.loginfo('Mode not valid')
+
